571d300f4e570afe6c9b4231308292a9d8dbef04
aprilhgshin <aprilhgshin@gmail.com>
2020-07-29 14:19:42 -0700

    field names modified

--- MITgcm_configurations/global_ocean.90x40x15/code_newPkg/USER_INPUT.h
+++ MITgcm_configurations/global_ocean.90x40x15/code_newPkg/USER_INPUT.h
@@ -5,7 +5,18 @@ C     *===============================================================*
 C
 C     nOB_mask  :: Number of open boundary masks.
 C     nOB_fld  :: Number of fields.
-C
+C     ob_fnames :: Filenames of each open boundary mask
+C     ob_hydrogSalt :: logical value of whether salinity field should be outputted
+C     ob_hydrogTheta :: logical value of whether theta field should be outputted
+C     ob_bathymetry :: logical value of whether bathymetry field should be outputted
 
       INTEGER, PARAMETER :: nOB_mask =  1
       INTEGER, PARAMETER :: nOB_fld = 2
+
+      LOGICAL ob_hydrogSalt
+      LOGICAL ob_hydrogTheta
+      LOGICAL ob_bathymetry
+      PARAMETER (
+     &           ob_bathymetry = .TRUE.,
+     &           ob_hydrogSalt = .FALSE.,
+     &           ob_hydrogTheta = .TRUE.)
