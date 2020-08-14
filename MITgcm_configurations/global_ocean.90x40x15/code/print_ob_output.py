9215b9b517c4e6026b84ff216ec93c4bc36a5763
aprilhgshin <aprilhgshin@gmail.com>
2020-08-14 15:09:30 -0700

    Plots

--- MITgcm_configurations/global_ocean.90x40x15/code/print_ob_output.py
+++ MITgcm_configurations/global_ocean.90x40x15/code/print_ob_output.py
@@ -11,12 +11,18 @@ import MITgcmutils as mitgcm
 if __name__ == "__main__":
 
 
+
     output_dir = Path('/home/mitgcm/Work/JPL_SHIN2020/MITgcm_configurations/global_ocean.90x40x15/run/')
     field = []
+    arr = np.zeros(45)
+    newfield=[]
 
-#    field = mitgcm.rdmds(str(output_dir / 'SALT'), itrs=-1,rec=1, fill_value=0.0)
+#    field = mitgcm.rdmds(str(output_dir / 'THETA'), itrs=-1,fill_value=0.0)
 #    print(np.fromfile(str(output_dir / 'THETA.001.001.data'), dtype='>f4').shape)
-    field = (np.fromfile(str(output_dir / 'SALT.data'), dtype='>f4').reshape(40,90))
+    field2 = (np.fromfile(str(output_dir / 'THETA.data'), dtype='>f4'))
+    print(field2.shape)
+#    newfield = (np.append(arr,field2)).reshape(40,90)
+#    print(newfield)
 #    lines = open(str(output_dir / 'THETA   _on_mask1_global.    122400.meta'))
 #    print(lines)
     #print(field.shape)
@@ -27,14 +33,12 @@ if __name__ == "__main__":
 
 
 
+#    plt.close(1)
+#    plt.figure(num=1,clear=True, figsize=(7,6))
+#    plt.subplot(211)
+#    plt.imshow(field, origin='lower')#, vmin=-2, vmax=16)
+#    plt.colorbar()
+#    plt.title('ETAH on Open Boundary Points')
 
-
-
-    plt.close(1)
-    plt.figure(num=1,clear=True, figsize=(7,6))
-    plt.subplot(211)
-    plt.imshow(field, origin='lower')#, vmin=-2, vmax=16)
-    plt.colorbar()
-    plt.title('ETAN on Open Boundary Points')
-
-    plt.show()
+#    plt.show()
+#
